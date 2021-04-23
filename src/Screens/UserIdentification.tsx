e88b56f8beaea2163e1b71c1fe586337c7d5655c
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-04-23 17:51:42 -0300

    feat: add escopo

--- src/Screens/UserIdentification.tsx
+++ src/Screens/UserIdentification.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, View } from 'react-native';
 import colors from '../styles/colors';
 
 const UserIdentification = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello User</Text>
+      <View style={styles.wrapper}>
+        <Text>Emoji aqui</Text>
+        <Text>Como podemos chamar você ?</Text>
+
+        <Text>Digite um nome</Text>
+        <Text>______________</Text>
+
+        <Text>Botao verde</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: colors.background,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
